ca0f0179046397b35e13a2c66adab6a4ea9a8ce2
Wendy123-akinyi <68918961+Wendy123-akinyi@users.noreply.github.com>
2020-09-05 05:04:17 +0300

    Add files via upload

---
+++ Project description.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Growth in Nairobi
+
+# Since time immemorial, Nairobi has been considered a cool city. Investors have since then been flocking this city in a bid to invest their money in diverse and multi-miliion projects. One challenge that has been faced though is the fact that some investments are not ideal in Nairobi. This project aims at trying to provide the best possible investment projects that can be carried out by both local and foreign investores in order for the city to grow economically.
+
+# In[ ]:
+
+
+
+
